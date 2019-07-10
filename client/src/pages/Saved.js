b43e54df15ef47a1554411b454de2744bfa9586d
whitney227 <whitneylcrawford@gmail.com>
2019-07-10 13:20:30 -0400

    saved page not rendering

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,5 +1,26 @@
 import React from "react";
+import {Container, Row, Col} from "../components/Grid";
+import Jumbotron from "../components/Jumbotron";
+import Button from "../components/Button";
+import Input from "../components/Input";
+import {BookList, BookListItem } from "../components/BookList"
 
-const Saved = () => <h1>Saved</h1>
+
+function Saved() {
+    return (
+        <div>
+            <Jumbotron>
+                <h1>My Saved Books</h1>
+            </Jumbotron>
+            <Container>
+                <Row>
+                    <Col size="md-12">
+                        
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
+}
 
 export default Saved;
