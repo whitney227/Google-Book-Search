2a88a440e017e110e0be2d0f046f7e87883e6663
whitney227 <whitneylcrawford@gmail.com>
2019-07-10 12:44:09 -0400

    search page complete

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -3,6 +3,7 @@ import {Container, Row, Col} from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
 import Button from "../components/Button";
 import Input from "../components/Input";
+import {BookList, BookListItem } from "../components/BookList"
 
 class Search extends Component {
     state={
@@ -30,29 +31,49 @@ class Search extends Component {
                         <Col size="md-12">
                             <form>
                             <Container>
-                  <Row>
-                    <Col size="xs-9 sm-10">
-                      <Input
-                        name="bookSearch"
-                        value={this.state.bookSearch}
-                        onChange={this.handleInputChange}
-                        placeholder="Search For a Book"
-                      />
-                    </Col>
-                    <Col size="xs-3 sm-2">
-                      <Button
-                        onClick={this.handleFormSubmit}
-                        type="success"
-                        className="input-lg"
-                      >
-                        Search
-                      </Button>
-                    </Col>
-                  </Row>
-                </Container>
-
+                              <Row>
+                                <Col size="xs-9 sm-10">
+                                  <Input
+                                    name="bookSearch"
+                                    value={this.state.bookSearch}
+                                    onChange={this.handleInputChange}
+                                    placeholder="Search For a Book"
+                                  />
+                                </Col>
+                                <Col size="xs-3 sm-2">
+                                  <Button
+                                    onClick={this.handleFormSubmit}
+                                    type="success"
+                                    className="input-lg"
+                                  >
+                                    Search
+                                  </Button>
+                                </Col>
+                              </Row>
+                            </Container>
                             </form>
                         </Col>
+                    </Row>
+                    <Row>
+                      <Col size="xs-12">
+                        {!this.state.books.length ? (
+                          <h1 className="text-center">No Books to Display</h1>
+                        ) : (
+                          <BookList>
+                            {this.state.books.map(book => {
+                              return (
+                                <BookListItem
+                                  key={book.title}
+                                  title={book.title}
+                                  description={book.description}
+                                  thumbnail={book.thumbnail}
+                                  link={book.lin}
+                                />
+                              );
+                            })}
+                          </BookList>
+                        )}
+                      </Col>
                     </Row>
 
                 </Container>
